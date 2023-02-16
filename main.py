ff5fe30ad5e9184661cf5485b20a66fb7e2cabd8
SGK <55636018+SGKespace@users.noreply.github.com>
2023-02-16 12:33:49 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+django.setup()
+
+from datacenter.models import Passcard  # noqa: E402
+
+if __name__ == '__main__':
+    # Программируем здесь
+    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    print(Passcard.objects.all())
+    print(Passcard.objects.all()[0])
+
+
