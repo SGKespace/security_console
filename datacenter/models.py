f57261c412c582861cd2006a75de72ba3c464b9a
SGK <55636018+SGKespace@users.noreply.github.com>
2023-02-16 12:38:18 +0400

    Create models.py

---
+++ datacenter/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class Passcard(models.Model):
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now=True)
+    passcode = models.CharField(max_length=200, unique=True)
+    owner_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        if self.is_active:
+            return f'owner_name: {self.owner_name} passcode: {self.passcode} created_at: {self.created_at} is_active: {self.is_active}'
+
+            # return self.owner_name + '  ' + self.passcode
+        return f'{self.owner_name} (inactive)'
+
+
+class Visit(models.Model):
+    created_at = models.DateTimeField(auto_now=True)
+    passcard = models.ForeignKey(Passcard, on_delete=models.CASCADE)
+    entered_at = models.DateTimeField()
+    leaved_at = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return '{user} entered at {entered} {leaved}'.format(
+            user=self.passcard.owner_name,
+            entered=self.entered_at,
+            leaved=(
+                f'leaved at {self.leaved_at}'
+                if self.leaved_at else 'not leaved'
+            )
+        )
